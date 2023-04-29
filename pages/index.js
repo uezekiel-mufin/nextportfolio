265f2434fc0d9977f6c3d10612b9dd800150b6f1
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-04-29 14:20:56 +0100

    welcome message

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { Typewriter } from 'react-simple-typewriter';
 
 export default function Home() {
+	useEffect(() => {
+		const message = new SpeechSynthesisUtterance('Hello, Welcome to my portfolio website, we are glad to have you here!');
+		window.speechSynthesis.speak(message);
+	});
 	return (
 		<motion.div transiton={{ duration: 5 }} initial={{ opacity: 0 }} animate={{ opacity: 1 }} className='w-full md:mx-8 h-screen z-[9999] flex justify-center items-center'>
 			<main className='mx-1 flex flex-col w-full md:w-3/5 md:h-3/5 gap-8 justify-center items-center text-center'>
@@ -15,7 +19,6 @@ export default function Home() {
 						<Typewriter words={['Ezekiel Udiomuno']} deleteAllOnFinish={true} cursor cursorStyle='|' loop={true} typeSpeed={200} deleteSpeed={200} delaySpeed={1000} />
 						<span className='text-[#944d97]'>!</span>
 					</div>
-					{/* Hi, My name is Ezekiel Udiomuno<span className='text-red-800'>!</span> */}
 				</h1>
 				<h3 className='text-[#17161A] font-semibold text-2xl md:text-4xl w-4/5'>I Develop Responsive and User-friendly Websites & Web-Applications</h3>
 
