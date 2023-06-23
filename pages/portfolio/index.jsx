74824326a1ae4c564eb433c20b2a6425b9f08b6d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-06-23 11:38:08 +0100

    Change portfolio item shadow

--- pages/portfolio/index.jsx
+++ pages/portfolio/index.jsx
@@ -17,14 +17,14 @@ const Portfolio = () => {
 			<div className='flex justify-center items-center h-full  '>
 				<div className='relative flex flex-col m-1 gap-12 md:grid md:grid-cols-3  md:gap-8 w-4/5 h-full justify-evenly content-evenly'>
 					{data.map((item) => (
-						<div key={item.id} className='flex shadow-lg flex-col gap-[5px] h-full justify-start shadow-pink-800 rounded-lg p-4 '>
+						<div key={item.id} className='relative grid grid-cols-1 shadow-lg gap-[5px] h-full justify-start shadow-pink-800 rounded-lg p-4 '>
 							<a href={item.liveDemo} target='_blank' rel='noreferrer' className='hover:scale-105 transition-all duration-500 ease-linear'>
 								<Image src={item.image} alt='image' className='rounded-md hover:scale-105' />
 							</a>
 							<h1 className='text-shadow font-bold'>{item.name}</h1>
 							<h4 className=' flex justify-start font-semibold'>{item.technologies}</h4>
 							<p className='text-sm'>{item.description}</p>
-							<div className=' flex justify-between mt-4 mb-2 w-full gap-4'>
+							<div className='flex justify-between mt-4 mb-2 w-full gap-4'>
 								<a href={item.liveDemo} rel='noreferrer' target='_blank' className='w-full'>
 									<button className='bg-[#944d97] rounded-md text-white w-full px-2  md:px-3 py-2 transition-all duration-500 ease-out hover:font-bold hover:bg-[#662768]'>live Demo</button>
 								</a>
