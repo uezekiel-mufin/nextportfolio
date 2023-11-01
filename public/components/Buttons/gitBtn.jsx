5691d8f0e2d7b69733fe5b5b9d88564f0f3822f0
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-11-01 16:46:09 +0100

    Refactor portfolio layout

--- public/components/Buttons/gitBtn.jsx
+++ public/components/Buttons/gitBtn.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { toast } from 'react-toastify';
 
 const GitBtn = ({ github }) => {
+	const notify = () => toast.info('This is a private repository');
 	return (
 		<>
 			{github ? (
@@ -11,7 +12,7 @@ const GitBtn = ({ github }) => {
 					<span className='relative text-black group-hover:text-white text-lg'>Github</span>
 				</a>
 			) : (
-				<button className='relative cursor-pointer w-full md:w-[150px] flex justify-center items-center px-4 py-3 font-medium group'>
+				<button onClick={notify} className='relative cursor-pointer w-full md:w-[150px] flex justify-center items-center px-4 py-3 font-medium group'>
 					<span className=' absolute inset-0 w-full h-full transition duration-300 ease-out transform translate-x-1 translate-y-1 bg-[#3b5998] group-hover:-translate-x-0 group-hover:-translate-y-0'></span>
 					<span className='absolute inset-0 w-full h-full bg-white border-2 border-[#3b5998] group-hover:bg-[#3b5998]'></span>
 					<span className='relative text-black group-hover:text-white text-lg'>Github</span>
