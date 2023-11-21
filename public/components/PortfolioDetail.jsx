374516be1fc686ed732a621b8c395568f55ed0a3
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-11-21 15:19:12 +0100

    Fix button height

--- public/components/PortfolioDetail.jsx
+++ public/components/PortfolioDetail.jsx
@@ -8,7 +8,8 @@ import DemoBtn from './Buttons/demoBtn';
 
 const ProjectDetails = ({ project }) => {
 	return (
-		<div className=''>
+		<div className='flex  flex-col justify-between h-full'>
+			<div>
 			<div className='flex flex-col gap-2 w-full items-start justify-between'>
 				<h3 className='text-2xl  font-bold'>{project?.name}</h3>
 			</div>
@@ -18,6 +19,7 @@ const ProjectDetails = ({ project }) => {
 			</div>
 			<div className='my-1'>
 				<p className='text-gray-600 text-base '>{project?.description}</p>
+			</div>
 			</div>
 			<div className='flex flex-row  mt-4 mb-2 w-full gap-4 '>
 				<GitBtn github={project?.Github} />
